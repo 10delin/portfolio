8bb5791eb3581c259b40aec1c5b72468294e74e5
Antonio Delage <delincruzado_20@hotmail.com>
2023-01-14 15:03:42 +0100

    feat(header): add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__content">
+        <div className="header__title">
+          <p>Antonio Delage</p>
+        </div>
+        <div className="header__description">
+          <p className="header__description__items">
+            <a href="#experience">Experiencia</a>
+          </p>
+          <p className="header__description__items">
+            <a href="#about">Sobre mí</a>
+          </p>
+          <p className="header__description__items">
+            <a href="#contact">Contacto</a>
+          </p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
