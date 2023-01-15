da34dfcd7b37e6d744948040b9ea71db45531201
Antonio Delage <delincruzado_20@hotmail.com>
2023-01-15 18:08:42 +0100

    feat(configure-project): add model file

---
+++ src/utils/model.js
@@ -0,0 +1,77 @@
+export const iconWorks = [
+  {
+    id: 1,
+    icon: "https://img.icons8.com/color/48/000000/javascript.png",
+    title: "javascript",
+    alt: "javascript",
+  },
+  {
+    id: 2,
+    icon: "https://img.icons8.com/color/48/000000/react-native.png",
+    title: "react",
+    alt: "react",
+  },
+  {
+    id: 3,
+    icon: "https://img.icons8.com/color/48/000000/html-5--v1.png",
+    title: "html",
+    alt: "html",
+  },
+  {
+    id: 4,
+    icon: "https://img.icons8.com/color/48/000000/css3.png",
+    title: "css",
+    alt: "css",
+  },
+  {
+    id: 5,
+    icon: "https://img.icons8.com/color/48/000000/sass.png",
+    title: "sass",
+    alt: "sass",
+  },
+  {
+    id: 6,
+    icon: "https://img.icons8.com/color/48/000000/nodejs.png",
+    title: "nodejs",
+    alt: "nodejs",
+  },
+  {
+    id: 7,
+    icon: "https://img.icons8.com/color/48/000000/redux.png",
+    title: "redux",
+    alt: "redux",
+  },
+  {
+    id: 8,
+    icon: "https://img.icons8.com/color/48/000000/git.png",
+    title: "git",
+    alt: "git",
+  },
+  {
+    id: 9,
+    icon: "https://img.icons8.com/color/48/000000/github--v1.png",
+    title: "github",
+    alt: "github",
+  },
+  {
+    id: 11,
+    icon: "https://img.icons8.com/color/48/000000/figma--v1.png",
+    title: "figma",
+    alt: "figma",
+  },
+];
+
+export const sendIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+  >
+    <path fill="none" d="M0 0h24v24H0z"></path>
+    <path
+      fill="currentColor"
+      d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+    ></path>
+  </svg>
+);
