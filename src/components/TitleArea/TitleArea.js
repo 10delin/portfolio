7a07a7726b86cedd83a81b208a3efe9f89ce5c54
Antonio Delage <delincruzado_20@hotmail.com>
2023-01-20 21:52:33 +0100

    feat(home): add new component titleArea,styles scss

---
+++ src/components/TitleArea/TitleArea.js
@@ -0,0 +1,10 @@
+const TitleArea = ({ title }) => {
+  return (
+    <div className="title-area">
+      <p>{title}</p>
+      <div className="title-area__line"></div>
+    </div>
+  );
+};
+
+export default TitleArea;
